8b5d03148f3583943cdb3f297c5bee9b743aa404
valeriia-nurieva <valerka.0906@gmail.com>
2022-09-25 08:02:29 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,15 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import '../css/common.css';
+import '../css/01-gallery.css';
+import createImgCard from './gallery-markup';
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 
 const markup = createImgCard(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", markup);
-
-function createImgCard(array) {
-  return array
-    .map(
-      ({ preview, original, description }) =>
-        `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a> `
-    )
-    .join("");
-}
 
 new SimpleLightbox('.gallery__item', { 
   captionsData: 'alt',
