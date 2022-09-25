56b74b58fc186d834b4920bdc1da71ab32d6ac0b
valeriia-nurieva <104796029+valeriia-nurieva@users.noreply.github.com>
2022-09-25 04:29:29 +0000

    Deploying to gh-pages from @ valeriia-nurieva/goit-js-hw-08@28ecd479002a9ffb78b778dedd45c57668f5c869 🚀

--- 03-feedback.3bbb7d65.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".feedback-form");console.log(o),console.log(o)}();
-//# sourceMappingURL=03-feedback.3bbb7d65.js.map
--- 03-feedback.b7ebb3c7.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".feedback-form");console.log(o),console.log(o);
-//# sourceMappingURL=03-feedback.b7ebb3c7.js.map
