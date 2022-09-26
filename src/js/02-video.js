bcadf9052f99f9da077b513862e4299fee15a2b8
valeriia-nurieva <valerka.0906@gmail.com>
2022-09-26 09:24:00 +0300

    Update 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
+
+const videoEl = document.querySelector('#vimeo-player');
+const LOCALE_STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(videoEl);
+
+initPage();
+    
+const onPlay = function(data) {
+    save(LOCALE_STORAGE_KEY, data);
+};
+
+const throttledOnTimeUpdate = throttle(onPlay, 1000);
+player.on('timeupdate', throttledOnTimeUpdate)
+
+function initPage() {
+    const saveData = load(LOCALE_STORAGE_KEY);
+    const { seconds } = saveData;
+  if (!saveData) {
+    return;
+  }
+    player.setCurrentTime(seconds);
+}
 
